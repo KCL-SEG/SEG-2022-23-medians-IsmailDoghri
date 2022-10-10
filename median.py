ec03c00b2604f424e53afc0461ef0089d1bf88b3
Ismail Doghri <ismail.doghri@kcl.ac.uk>
2022-10-10 13:51:41 +0100

    Final solution

--- median.py
+++ median.py
@@ -1,16 +1,20 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-from statistics import median
-
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
-        list =[]
+        initList = []
         for value in input().split(","):
-            list.append(float(value))
+            initList.append(float(value))
+            lenList = len(initList)
+            sortedList = sorted(initList)
+        if lenList % 2:
+            median = sortedList[(lenList - 1) // 2]
+        else:
+            median = (sortedList[lenList // 2] + sortedList[(lenList // 2) - 1]) / 2
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(median(list))
+print(median)
